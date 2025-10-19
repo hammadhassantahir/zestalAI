a48cbd8bfdc97f7601f063db30e09c05130b9725
hammadhassantahir <hammad.hassan.infinity@gmail.com>
2025-10-20 04:25:47 +0500

    quick Updates

--- app/script/scrapper.py
+++ app/script/scrapper.py
@@ -22,7 +22,7 @@ def get_driver():
     Create and return a configured Selenium WebDriver
     """
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
